1d1362ee9deb219d3b0aa1884a9de5cb129f768c
alexandriaross <alexandria.ross@gmail.com>
2020-02-01 16:17:37 -0500

    Add files via upload

---
+++ module3.R
@@ -0,0 +1,6 @@
+Name <-c("Jeb","Donald","Ted","Marco","Carly","Hillary","Bernie")
+ABC_pollresults <- c(4, 62, 51, 21, 2, 14, 15)
+CBS_pollresults <- c(12, 75, 43, 19, 1, 21, 19)
+PollresultData <- data.frame(Name, ABC_pollresults, CBS_pollresults)
+str(PollresultData)
+PollresultData
